45b24c42ff4984597bb0e813a07bdbf7d2d9a1fb
Minuk Kim <mkim01@stuy.edu>
2019-02-14 11:09:05 -0500

    worked in class

---
+++ KnightBoard.java
@@ -0,0 +1,74 @@
+
+public class KnightBoard{
+  private int[][] board;
+  public KnightBoard(int startingRows,int startingCols){
+    board = new int [startingRows][startingCols];
+    if (startingRows <= 0 || startingCols <= 0){
+      throw new IllegalStateException();
+    }
+  }
+
+  public String toString(){
+    String result = "";
+    for(int i = 0 ; i < startingRows; i++){
+      result += "\n";
+      for(int j = 0; j < startingCols; j++){
+        if(board[i][j] == ){;
+        }
+        else{
+          board[i][j]
+        }
+      }
+    }
+    return "";
+  }
+
+  public boolean addKnights(int row, int col){
+    if (board[row][col] == 0){
+      board[row][col] = -1;
+      return true;
+    }
+      return false;
+    }
+
+  public boolean removeKnights(int row, int col){
+    if (board[row][col] == 1){
+      board[row][col] == 0;
+      return true;
+    }
+    return false;
+  }
+
+  public boolean solve(int startingRow, int startingCol){
+    for(int i = 0; i < startingRow; i++){
+      for(int j = 0; j< startingCol; j++){
+        if (board[i][j] != 0){
+          throw new IllegalStateException();
+        }
+      }
+    }
+    if (startingRow < 0 || startingCol < 0){
+      return false;
+    }
+      return solveH(startingrow, startingCol);
+    }
+
+  private boolean solveH(int row, int col, int moveNumber){
+    if (moveNumber == 0){return true};
+    if (addKnights(row,col)){
+      if (solveH(row + 1, col + 2, moveNumber - 1) || solveH(row + 2, col + 1, moveNumber - 1)
+          || solveH(row - 1, col - 2, moveNumber - 1) || solveH(row - 2, col - 1, moveNumber - 1)){
+            return true;
+          }
+      else{
+        removeKnights(row, col);
+      }
+    }
+      return false;
+  }
+
+  public int countSolutions(int startingRow, int startingCol){
+      return 0;
+
+
+    }
