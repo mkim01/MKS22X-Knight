2d17ac6a74ab8e60b36af45ad35a4f4b205d42ea
Minuk Kim <mkim01@stuy.edu>
2019-02-19 13:12:02 -0500

    general algo for solveH

--- KnightBoard.java
+++ KnightBoard.java
@@ -2,6 +2,9 @@
 public class KnightBoard{
   private int[][] board;
   private int order = 1;
+  private int area = board.length * board[0].length;
+  private int posXmove[8] = {1, -1, -2, -2, -1,  1, 2,  2};
+  private int posYmove[8] = {2,  2,  1, -1, -2, -2, 1, -1};
 
   public KnightBoard(int startingRows,int startingCols){
     board = new int [startingRows][startingCols];
@@ -50,33 +53,38 @@ public class KnightBoard{
   }
 
   public boolean solve(int startingRow, int startingCol){
-    // for(int i = 0; i < startingRow; i++){
-    //   for(int j = 0; j< startingCol; j++){
-    //     if (board[i][j] != 0){
-    //       throw new IllegalStateException();
-    //     }
-    //   }
-    // }
-    // if (startingRow < 0 || startingCol < 0){
-    //   return false;
-    // }
-    //   return solveH(startingrow, startingCol);
-    return false;
+    for(int i = 0; i < startingRow; i++){
+      for(int j = 0; j< startingCol; j++){
+        if (board[i][j] != 0){
+          throw new IllegalStateException();
+        }
+      }
+    }
+    if (startingRow =< 0 || startingCol =< 0){
+      throw new IllegalArgumentException();
+    }
+    //  return solveH(startingrow, startingCol, area);
+      return false;
     }
 
   private boolean solveH(int row, int col, int moveNumber){
-    // if (moveNumber == 0){return true}
-    // if (addKnights(row,col)){
-    //   if (solveH(row + 1, col + 2, moveNumber - 1) || solveH(row + 2, col + 1, moveNumber - 1)
-    //       || solveH(row - 1, col - 2, moveNumber - 1) || solveH(row - 2, col - 1, moveNumber - 1)){
-    //         return true;
-    //       }
-    //   else{
-    //     removeKnights(row, col);
-    //   }
+    // if (row >= board.length || row <= 0 || col >= board[0].length || col <= 0){
+    //       return false;
     // }
+    if (moveNumber == area){
+      return true;
+    }
+    for(int i = 0; i < 8; i++){
+     if (addKnights(row, col)){
+        solveH(row + posXmove[i], col + posYmove[i], moveNumber + 1){
+      }
+    else{
+      removeKnights(row,col);
+      }
+    }
       return false;
   }
+}
 
   public int countSolutions(int startingRow, int startingCol){
       return 0;
