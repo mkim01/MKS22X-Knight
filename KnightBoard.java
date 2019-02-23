7da3b9d34dd9cc917c19bb54fdc9eb1f2cd9b703
Minuk Kim <mkim01@stuy.edu>
2019-02-22 20:41:43 -0500

    get rid of area variable for nullpointerexcpetion'

--- KnightBoard.java
+++ KnightBoard.java
@@ -2,7 +2,7 @@
 public class KnightBoard{
   private int[][] board;
   private int order = 1;
-  private int area = board.length * board[0].length;
+  //private int area = board.length * board[0].length;
   private int[] posXmove = {1, -1, -2, -2, -1,  1, 2,  2};
   private int[] posYmove = {2,  2,  1, -1, -2, -2, 1, -1};
 
@@ -21,21 +21,27 @@ public class KnightBoard{
   public String toString(){
     String result = "";
     for(int i = 0 ; i < board.length; i++){
-      result += "\n";
-      for(int j = 0; j < board[0].length; j++){
-        if (area >= 10 || board[i][j] < 10){
-          result += "" + board[i][j];
+      for(int j = 0; j < board[i].length; j++){
+        result += " ";
+        if (board[i][j] == 0){
+          result += "_";
         }
         else{
-          result += " "+ board[i][j];
+          if(board.length * board[0].length >= 10 || board[i][j] < 10){
+            result += "" + board[i][j];
+          }
+          else{
+            result += " " + board[i][j];
+          }
+        }
       }
+        result += "\n";
     }
-  }
-    return result;
+      return result;
   }
 
   public boolean addKnights(int row, int col){
-    if (row <= 0 || col <= 0){
+    if (row < 0 || col < 0 || row > board.length || col > board[0].length){
       throw new IllegalArgumentException();
     }
     if (board[row][col] == 0){
@@ -47,55 +53,54 @@ public class KnightBoard{
     }
 
   public boolean removeKnights(int row, int col){
-    if (row <= 0 || col <= 0){
+    if (row < 0 || col < 0){
       throw new IllegalArgumentException();
     }
     if (board[row][col] != 0){
       board[row][col] = 0;
+      order --;
       return true;
     }
     return false;
   }
 
   public boolean solve(int startingRow, int startingCol){
-    for(int i = 0; i < startingRow; i++){
-      for(int j = 0; j< startingCol; j++){
-        if (board[i][j] != 0){
-          throw new IllegalStateException();
-        }
-      }
-    }
-    if (startingRow <= 0 || startingCol <= 0){
-      throw new IllegalArgumentException();
-    }
+    // for(int i = 0; i < startingRow; i++){
+    //   for(int j = 0; j< startingCol; j++){
+    //     if (board[i][j] != 0){
+    //       throw new IllegalStateException();
+    //     }
+    //   }
+    // }
+    // if (startingRow < 0 || startingCol < 0){
+    //   throw new IllegalArgumentException();
+    // }
     //  return solveH(startingrow, startingCol, area);
-      return false;
+      return solveH(startingRow, startingCol, 0);
     }
 
   private boolean solveH(int row, int col, int moveNumber){
-    // if (row >= board.length || row <= 0 || col >= board[0].length || col <= 0){
-    //       return false;
-    // }
-    if (moveNumber == area){
+    if (moveNumber == board.length * board[0].length){
       return true;
     }
-    for(int i = 0; i < 8; i++){
-     if (addKnights(row, col)){
-        solveH(row + posXmove[i], col + posYmove[i], moveNumber + 1);
-      }
-    else{
-      removeKnights(row,col);
-      }
+    for (int i = 0; i < 8; i++){
+      if (addKnights(row, col)){
+        if (solveH(row + posXmove[i], col + posYmove[i], moveNumber + 1)){
+            return true;
+          }
+        removeKnights(row, col);
+        }
     }
-      return false;
+    return false;
   }
 
   public int countSolutions(int startingRow, int startingCol){
       return 0;
     }
 
-  // public static void main(String[] args){
-  //   KnightBoard board = new KnightBoard(5,5);
-  //   System.out.println(board);
-  // }
+   public static void main(String[] args){
+    KnightBoard board = new KnightBoard(5,5);
+  //  System.out.println(board.solve(0,0));
+    System.out.println(board);
+  }
 }
